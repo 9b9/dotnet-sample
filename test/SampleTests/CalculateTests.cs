d4571dd5398f7ce605f7fcb2f7e7ed62e63cf424
9b9 <b951852753@gmail.com>
2022-10-10 22:20:59 +0800

    test: change test case

--- test/SampleTests/CalculateTests.cs
+++ test/SampleTests/CalculateTests.cs
@@ -3,7 +3,7 @@ namespace SampleTests;
 public class Tests
 {
     [TestCase(1, 2, 3)]
-    [TestCase(2, 2, 5)]
+    [TestCase(2, 2, 4)]
     public void Add_AddTwoValue_ReturnsSum(int a, int b, int expected)
     {
         // Arrange
